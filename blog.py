30c4b38f9aebcf1f7eac7e0a294f0adb6c4b03d6
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-05-08 14:52:23 +0530

    added configs in the flask app itself instead of environment variables

--- blog.py
+++ blog.py
@@ -3,5 +3,12 @@ from flask import Flask
 app = Flask(__name__) # name means name of this module which is main
 
 @app.route("/")
-def hello_world():
+def home():
     return "<p>Hello, World!</p>"
+
+@app.route("/about")
+def about():
+    return "<p>ABout!</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
