0d211258549e2e452152435b86d5cc4353134e5c
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-05-09 16:39:24 +0530

    Added templates using template inheritance and jinja

--- blog.py
+++ blog.py
@@ -1,14 +1,46 @@
-from flask import Flask
+from flask import Flask , render_template , url_for
 
 app = Flask(__name__) # name means name of this module which is main
 
+posts = [
+    {
+        "author":"Arnab",
+        "title" : "Blog post",
+        "content" : "First",
+        "date" : "sldkjfl"
+    },
+    {
+        "author":"Arnab",
+        "title" : "Blog post",
+        "content" : "First",
+        "date" : "sldkjfl"
+    },
+    {
+        "author":"Arnab",
+        "title" : "Blog post",
+        "content" : "First",
+        "date" : "sldkjfl"
+    },
+    {
+        "author":"Arnab",
+        "title" : "Blog post",
+        "content" : "First",
+        "date" : "sldkjfl"
+    },
+    {
+        "author":"Arnab",
+        "title" : "Blog post",
+        "content" : "First",
+        "date" : "sldkjfl"
+    },
+]
 @app.route("/")
 def home():
-    return "<p>Hello, World!</p>"
+    return render_template("home.html",post = posts)
 
 @app.route("/about")
 def about():
-    return "<p>ABout!</p>"
+    return render_template("about.html",title="about")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- readme.md
+++ readme.md
@@ -3,3 +3,6 @@
 FLASK_APP=app.py
 export FLASK_DEBUG=True 
 flask run
+
+## Template Interheritance
+layout.html is the parent template.
